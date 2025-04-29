45b4c0191ae0703ffc3e9842355adc50371ba63c
saketkakkar-101 <saketkakkar55555@gmail.com>
2025-04-29 18:30:54 +0530

    practice

--- frontend/src/auth/forms/SignUpForm.jsx
+++ frontend/src/auth/forms/SignUpForm.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { z } from "zod"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { z } from "zod";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,153 +13,170 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
- 
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 const formSchema = z.object({
-  username: z.string().min(2, {message: "Username must be atleast 2 characters"}),
-  email: z.string().min({message: "Invalid email address"}),
-  password: z.string().min(8, {message: "Password must be atleast 8 characters"}),
-})
-
-
+  username: z
+    .string()
+    .min(2, { message: "Username must be atleast 2 characters" }),
+  email: z.string().min({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be atleast 8 characters" }),
+});
 
 const SignUpForm = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [loading, setLoading] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
-   // 1. Define your form.
-   const form = useForm({
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  // 1. Define your form.
+  const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
-      email:"",
-      password:""
+      email: "",
+      password: "",
     },
-  })
+  });
 
-    // 2. Define a submit handler.
-  async  function onSubmit(values) {
-      try {
-        setLoading(true)
-        setErrorMessage(null)
+  // 2. Define a submit handler.
+  async function onSubmit(values) {
+    try {
+      setLoading(true);
+      setErrorMessage(null);
 
-        const res = await fetch("/api/auth/signup" ,{
-          method: "POST",
-          headers : {"Content-Type": "application/json"},
-          body: JSON.stringify(values)
-        })
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      });
 
-        const data = await res.json();
+      const data = await res.json();
 
-        if (data.success === false) {
-          setLoading(false)
+      if (data.success === false) {
+        setLoading(false);
 
-          return setErrorMessage(data.message)
-        }
-
-        setLoading(false)
-
-        if(res.ok){
-         navigate("/sign-in")
-        }
-      } catch (error) {
-        setErrorMessage(error.message)
-        setLoading(false)
+        return setErrorMessage(data.message);
       }
+
+      setLoading(false);
+
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
     }
+  }
 
   return (
-    <div className='min-h-screen mt-20'>
-       <div className='flex p-3 max-w-3xl
-       sm:max-w-5xl mx-auto flex-col md:flex-row
-       md:items-center gap-5'>
-       {/* left */}
- 
-         <div className='flex-1'>
-          <Link to={"/"} className="font-bold 
-          text-2xl sm:text-4xl flex slex-wrap">
-            <span className='text-slate-500'>Morning</span>
-            <span className='text-slate-900'>Dispatch</span>
+    <div className="min-h-screen mt-20">
+      <div
+        className="flex p-3 max-w-3xl sm:max-w-5xl mx-auto flex-col md:flex-row md:items-center gap-5"
+      >
+        {/* left */}
+
+        <div className="flex-1">
+          <Link
+            to={"/"}
+            className="font-bold 
+          text-2xl sm:text-4xl flex flex-wrap"
+          >
+            <span className="text-slate-500">Morning</span>
+            <span className="text-slate-900">Dispatch</span>
           </Link>
-          <h2 className='text-[24px] 
-          md:text-[30px] font-bold leading-[140%]
-          tracking-tighter pt-5 sm:pt-12'>Create a new account
+          <h2
+            className="text-[24px] md:text-[30px] font-bold leading-[140%] tracking-tighter pt-5 sm:pt-12"
+          >
+            Create a new account
           </h2>
- 
-          <p className='text-slate-500 text-[14px]
+
+          <p
+            className="text-slate-500 text-[14px]
           font-medium leading-[140%] md:text-[16px] 
-          md:font-normal mt-2'>Welcome to Morning Dispatch, Please provide your details
+          md:font-normal mt-2"
+          >
+            Welcome to Morning Dispatch, Please provide your details
           </p>
-         </div>
-       
-       {/* right */}
+        </div>
 
+        {/* right */}
 
-          <div className='flex-1'>
+        <div className="flex-1">
           <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="Username" {...field} />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="Username" {...field} />
+                    </FormControl>
 
-<FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="xyz@email.com" {...field} />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="xyz@email.com"
+                        {...field}
+                      />
+                    </FormControl>
 
-<FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="Password" {...field} />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        {...field}
+                      />
+                    </FormControl>
 
-        <Button type="submit" className="bg-blue-500 w-full">Submit</Button>
-      </form>
-    </Form>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-    <div className='flex gap-2 text-sm mt-5'>
-      <span>Have an account?</span>
-      <Link to="/sign-in" className='text-blue-500'>Sign In</Link>
-    </div>
+              <Button type="submit" className="bg-blue-500 w-full">
+                Submit
+              </Button>
+            </form>
+          </Form>
+
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
           </div>
-
-       </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -7,13 +7,12 @@ const Header = () => {
     <div>
       <header className="shadow-lg">
         <div
-          className="flex justify-between items-center 
-    max-w-6xl lg:max-w-7xl mx-auto p-4"
+          className="flex justify-between items-center max-w-6xl lg:max-w-7xl 
+          mx-auto p-4"
         >
           <Link to={"/"}>
             <h1
-              className="font-bold text-xl sm:text-2xl flex
-        flex-wrap"
+              className="font-bold text-xl sm:text-2xl flex flex-wrap"
             >
               <span className="text-slate-500">Morning</span>
               <span className="text-slate-900">Dispatch</span>
@@ -21,25 +20,22 @@ const Header = () => {
           </Link>
 
           <form
-            className="p-3 bg-slate-100 rounded-lg 
-      flex items-center"
+            className="bg-slate-100 rounded-lg flex items-center p-3"
           >
             <input
               type="text"
               placeholder="Search..."
-              className="focus:outline-none bg-transparent w-24 
-        sm:w-64"
+              className="focus:outline-none bg-transparent w-24 sm:w-64"
             />
 
             <button>
               <FaSearch className="text-slate-600" />
             </button>
           </form>
-          <ul className="flex gap-4 ">
+          <ul className="flex gap-4">
             <Link to={"/"}>
               <li
-                className="hidden lg:inline text-slate-700 
-          hover:underline"
+                className="hidden lg:inline text-slate-700 hover:underline"
               >
                 Home
               </li>
